df678cca012757a79a5eed367a6b0b0d96c96cba
Brent Pedersen <bpederse@gmail.com>
2019-07-31 20:46:54 -0600

    working values and intervals see #1

--- src/bigwig.nim
+++ src/bigwig.nim
@@ -7,6 +7,18 @@ type BigWig* = ref object
   bw : ptr bigWigFile_t
   path: string
 
+
+type Stat* {.pure.} = enum
+    #doesNotExist = -1 #!< This does nothing */
+    mean = 0
+    stdev = 1
+    max = 2
+    min = 3
+    # The number of bases covered
+    coverage = 4
+    # The sum of per-base values */
+    sum = 5
+
 proc close*(bw: BigWig) =
   ## close the file and free up resources
   if bw.bw != nil:
@@ -24,3 +36,39 @@ proc open*(bw: var BigWig, path: string, mode: FileMode=fmRead): bool =
   bw = BigWig(bw: bwOpen(path, nil, fmode), path: path)
   return bw.bw != nil
 
+type CPtr[T] = ptr UncheckedArray[T]
+
+proc get_stop(bw: var BigWig, chrom: string, stop:int): int {.inline.} =
+  if stop >= 0 : return stop
+  let tid = bw.bw.bwGetTid(chrom)
+  if tid == uint32.high:
+    raise newException(KeyError, "[bigwig] unknown chromosome:" & chrom)
+  result = cast[CPtr[uint32]](bw.bw.cl.len)[tid].int
+
+proc values*(bw: var BigWig, values: var seq[float32], chrom: string, start:int=0, stop:int= -1, includeNA:bool=false) =
+  ## exctract values for the given range into `values`
+  var stop = bw.get_stop(chrom, stop)
+  var ivs = bwGetValues(bw.bw, chrom, start.uint32, stop.uint32, includeNA.cint)
+  values.setLen(ivs.l)
+  copyMem(values[0].addr, ivs.value, sizeof(values[0]) * ivs.l.int)
+  ivs.bwDestroyOverlappingIntervals()
+
+iterator intervals*(bw: var BigWig, chrom: string, start:int=0, stop:int= -1): tuple[start: int, stop: int, value: float32] =
+  ## iterate over the values in the given region
+  var stop = bw.get_stop(chrom, stop)
+  var it = bwOverlappingIntervalsIterator(bw.bw, chrom, start.uint32, stop.uint32, 1000)
+
+  while it.data != nil:
+    if it.entries != nil:
+      raise newException(ValueError, "iterator not implemented for bigbed")
+
+    elif it.intervals != nil:
+      let starts = cast[CPtr[uint32]](it.intervals.start)
+      let stops = cast[CPtr[uint32]](it.intervals.end)
+      let values = cast[CPtr[cfloat]](it.intervals.value)
+      for i in 0..<it.intervals.l:
+        yield (starts[i].int, stops[i].int, values[i])
+
+    it = bwIteratorNext(it)
+
+  it.bwIteratorDestroy
--- tests/test_read.nim
+++ tests/test_read.nim
@@ -1,7 +1,37 @@
 import unittest
 import bigwig
+import math
+
+proc `~~`(a: float32, b: float32): bool =
+  return abs(a - b) < 1e-9
 
 suite "test reading":
   test "that missing file returns false":
     var bw: BigWig
     check false == open(bw, "xxxxxx.bw")
+
+  test "that reading values works":
+    var bw: BigWig
+    check true == open(bw, "tests/test.bw")
+
+    var values: seq[float32]
+    bw.values(values, "1")
+    check values[0] ~~ 0.1
+    check values[1] ~~ 0.2
+    check values[2] ~~ 0.3
+
+  test "that interval iteration works":
+    var bw: BigWig
+    check true == open(bw, "tests/test.bw")
+    var expect : seq[tuple[start:int, stop:int, value:float32]] = @[
+        (start: 0, stop: 1, value: 0.1000000014901161'f32),
+        (start: 1, stop: 2, value: 0.2000000029802322'f32),
+        (start: 2, stop: 3, value: 0.300000011920929'f32),
+        (start: 100, stop: 150, value: 1.399999976158142'f32),
+        (start: 150, stop: 151, value: 1.5'f32)]
+
+    var i = 0
+    for iv in bw.intervals("1"):
+      check iv == expect[i]
+      i += 1
+
